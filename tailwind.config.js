e9048ec5d695107ec3804edc868a790b6ff38804
trentbrew <hello@trentbrew.com>
2025-06-14 21:58:42 -0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import { fontFamily } from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class', // Enable class-based dark mode
 	theme: {
 		extend: {
 			colors: {
